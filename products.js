be5e7ed26480453e885ba8dc50112b6d520dd2e9
Pankhuri Deshmukh <104618202+pankhuri-deshmukh@users.noreply.github.com>
2023-01-27 12:00:43 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,30 +1,30 @@
 const Products = [
     {
-        productName: 'Foldable Mini Drone',
+        productName: 'Simple Mini Dress',
         productNumber: '84937',
         quantity: '78',
         status: 'Pending'
     },
     {
-        productName: 'LARVENDER K678 Drone',
+        productName: 'LAVENDER K678 Cap',
         productNumber: '84698',
         quantity: '75',
         status: 'Declined'
     },
     {
-        productName: 'Ruko GPS Z437 Drone',
+        productName: 'Rubarb Motorcycle Jacket',
         productNumber: '42737',
         quantity: '167',
         status: 'Delivered'
     },
     {
-        productName: 'Pro 16MP Camera Drone',
+        productName: 'Pro Boyz Leather Coat',
         productNumber: '54795',
         quantity: '49',
         status: 'Pending'
     },
     {
-        productName: 'Ruko PA64 Drone',
+        productName: 'Red Dress',
         productNumber: '69954',
         quantity: '84',
         status: 'Delivered'
